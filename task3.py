bf53883bdb396a0b58b1c63cd76dafc8f8c7b587
Leonid Maslov <mlenkei@mail.ru>
2023-05-19 21:35:17 +0300

    task3

---
+++ task3.py
@@ -0,0 +1,6 @@
+
+firstClass = int(input('Введите количество учеников в 1 классе: '))
+secondClass = int(input("Введите кол учеников во втором классе: "))
+thirdClass = int(input("3 класс: "))
+
+print((firstClass//2 + secondClass//2 + thirdClass // 2)
