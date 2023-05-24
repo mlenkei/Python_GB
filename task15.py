db80d497b86d940c2495e5be27e1b1ca5bfd8fca
Leonid Maslov <mlenkei@mail.ru>
2023-05-24 21:03:44 +0300

    task15

--- task15.py
+++ task15.py
@@ -1,14 +1,14 @@
 
 n = int(input("Введите число арбузов: 5 "))
 ves = [3, 1, 2, 3, 9]
-max_ar = ves[0]
-min_ar = ves[0]
+max_arb = ves[0]
+min_arb = ves[0]
 
 for i in ves:
-    if i > max_ar:
-        max_ar = i
-    if i < min_ar:
-        min_ar = i
-print(max_ar)
-print(min_ar)
+    if i > max_arb:
+        max_arb = i
+    if i < min_arb:
+        min_arb = i
+print(max_arb)
+print(min_arb)
 
