3c2b7616ba4ef41d2a81a2bce5b5a9a52c67aafd
Leonid Maslov <mlenkei@mail.ru>
2023-05-28 18:28:15 +0300

    DZtask18

--- task18.py
+++ task18.py
@@ -9,23 +9,31 @@
 #     1 2 3 4 5
 #     6
 #     -> 5
+
 import random
 
-n = int(input("Введите N - колличество элементов в массиве: "))
-b = int(input("Введите число, которое Вы бы хотели проверить, в массиве есть ли оно и ближайшие к нему строки в массиве: "))
+n = int(input("Введите N - колличество элементов в массиве : "))
+
+b= b1 = b2 = int(input("Введите число, которое Вы бы хотели проверить, соответсвенно найдем ближайшие к нему строки в массиве: "))
 rand_list = []
-c=0
+c=1
 k=0
 
 for i in range(n):
-    rand_list.append(random.randint(0,10))
-
-min_z = abs(b - rand_list[0])
-print(rand_list, min_z)
-k = abs(b - rand_list[i])
-if k < min_z:
-    min_z = k      
-    c = i
-print("Ближайшее число к Вашему числу в массиве равно: ", rand_list[c], "их разница составляет: ", b - rand_list[c])
-print(rand_list)
-
+    rand_list.append(random.randint(0,100))
+try:
+    for i in range(n):
+        b1 = b1 - c   
+        b2 = b2 + c   
+        for i in range(n):
+            min_z = b1
+            max_z = b2
+            print(rand_list, min_z, max_z)
+            k = rand_list[i]
+            print(k, min_z, max_z)
+            if k == min_z or k == max_z:
+                print("Ближайшее число в этом массиве к ", b, "это число ", k) 
+                raise StopIteration
+except StopIteration:
+    pass
+ 
